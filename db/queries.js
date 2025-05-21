b159a7b7554447207242397ceb2c18eccda5bb56
basha105 <brentbasha17@gmail.com>
2025-05-20 20:05:37 -0700

    trying to refactor message functions

--- db/queries.js
+++ db/queries.js
@@ -3,3 +3,8 @@ const pool = require('./pool');
 async function createMessage(user, message) {
     await pool.query("INSERT INTO messages (user, message) VALUES ($1, $2)", [user, message]);
 }
+
+module.exports = {
+    createMessage
+}
+
